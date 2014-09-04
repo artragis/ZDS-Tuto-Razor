b671d4c9f41b55f696e27330186ec759751cdac0
zyhou <richard.maxime9@gmail.com>
2014-09-04 22:19:06 +0200

    Mise en place du projet Blog / ZDS

---
+++ Blog/Controllers/AccueilController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: Accueil
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /About/
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Blog/Models/Article.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Blog.Models
+{
+    /// <summary>
+    /// Notre modèle de base pour représenter un article
+    /// </summary>
+    public class Article
+    {
+        /// <summary>
+        /// Le pseudo de l'auteur
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128)]
+        [RegularExpression(@"^[^,\.\^]+$")]
+        [DataType(DataType.Text)]
+        public string Pseudo { get; set; }
+
+        /// <summary>
+        /// Le titre de l'article
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128)]
+        [DataType(DataType.Text)]
+        public string Titre { get; set; }
+
+        /// <summary>
+        /// Le contenu de l'article
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [DataType(DataType.Text)]
+        public string Contenu { get; set; }
+    }
+}
