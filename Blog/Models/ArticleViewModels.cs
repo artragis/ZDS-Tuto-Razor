d365d430398cce98dbcce4a725ef9c19d177f780
françois dambrine <artragis@gmail.com>
2014-09-05 11:26:33 +0200

    Ajout du viewmodel + typofix

--- Blog/Models/ArticleViewModels.cs
+++ Blog/Models/ArticleViewModels.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Web;
 
 namespace Blog.Models
 {
-    /// <summary>
-    /// Notre modèle de base pour représenter un article
-    /// </summary>
-    public class Article
+    public class ArticleCreation
     {
         /// <summary>
         /// Le pseudo de l'auteur
@@ -19,7 +17,6 @@ namespace Blog.Models
         [RegularExpression(@"^[^,\.\^]+$")]
         [DataType(DataType.Text)]
         public string Pseudo { get; set; }
-
         /// <summary>
         /// Le titre de l'article
         /// </summary>
@@ -27,12 +24,18 @@ namespace Blog.Models
         [StringLength(128)]
         [DataType(DataType.Text)]
         public string Titre { get; set; }
-
         /// <summary>
         /// Le contenu de l'article
         /// </summary>
         [Required(AllowEmptyStrings = false)]
         [DataType(DataType.MultilineText)]
         public string Contenu { get; set; }
+
+        /// <summary>
+        /// Le fichier image
+        /// </summary>
+        [DisplayName("Illustration")]
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase Image { get; set; }
     }
 }
